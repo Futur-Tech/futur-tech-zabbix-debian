a1b9ade8e2fb6da3fc4026001f82c664209bf92a
Guillaume Hullin <guillaume.hullin@futur-tech.com>
2021-02-20 01:04:53 +0100

    overide for Debian 7

--- deploy.sh
+++ deploy.sh
@@ -83,10 +83,10 @@ fi
 dpkg -i ${PKG_ZBX_NAME} &>/dev/null
 $S_LOG -s $? -d $S_NAME "DPKG of ${PKG_ZBX_NAME} returned code $?"
 
-apt update &>/dev/null
+apt-get update &>/dev/null
 $S_LOG -s $? -d $S_NAME "APT UPDATE returned code $?"
 
-apt --yes install zabbix-agent &>/dev/null
+apt-get --yes install zabbix-agent &>/dev/null
 $S_LOG -s $? -d $S_NAME "APT INSTALL of zabbix-agent returned code $?"
 
 
