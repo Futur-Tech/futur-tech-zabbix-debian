4b017140fedcd85d41ed621a0b9e6e56df64818c
Guillaume Hullin <guillaume.hullin@futur-tech.com>
2021-04-15 11:33:33 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -105,11 +105,8 @@ $S_LOG -s $? -d $S_NAME "Edited: $(egrep ^Server= $ZBX_CONF)"
 sed -i -r "s/^(ServerActive=127\.0\.0\.1)/ServerActive=$ZBX_SRV_ACTIVE/g" $ZBX_CONF
 $S_LOG -s $? -d $S_NAME "Edited: $(egrep ^ServerActive= $ZBX_CONF)"
 
-sed -i -r "s/^Include=\/etc\/zabbix\/zabbix_agentd.d\/*.conf$/Include=\/etc\/zabbix\/zabbix_agentd.conf.d\/*.conf/g" $ZBX_CONF
-$S_LOG -s $? -d $S_NAME "Edited for conf Zabbix dir: $(egrep ^Include= $ZBX_CONF)"
-
 sed -i -r "s/^Include=\/etc\/zabbix\/zabbix_agentd.d\/$/Include=\/etc\/zabbix\/zabbix_agentd.conf.d\/*.conf/g" $ZBX_CONF
-$S_LOG -s $? -d $S_NAME "Edited for old Zabbix version: $(egrep ^Include= $ZBX_CONF)"
+$S_LOG -s $? -d $S_NAME "Edited: $(egrep ^Include= $ZBX_CONF)"
 
 #############################
 #############################
